563900ee5aaf706502a65b1ab5484cdeb227f45d
Shreekant Kalwar <shreekantkalwar@gmail.com>
2025-04-10 11:06:33 +0530

    Cors Update

--- server/app.js
+++ server/app.js
@@ -1,28 +1,34 @@
 import express from 'express';
 import cors from 'cors';
 
-
 import authRoutes from './routes/auth.routes.js';
 import stockRoutes from './routes/stock.routes.js';
 import watchlistRoutes from './routes/watchlist.routes.js';
 
-
-
 const app = express();
 
-app.use(cors());
+// Explicitly allow frontend origin
+const allowedOrigins = ['https://stock-price-prediction-two.vercel.app'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server Running OK")
-})
+    res.send("Server Running OK");
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/stock', stockRoutes);
 app.use('/api/watchlist', watchlistRoutes);
-
-
-
 
 export default app;
